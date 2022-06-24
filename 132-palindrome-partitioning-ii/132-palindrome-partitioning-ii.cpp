2e3a21074020f02386d5297fa3e427845363975d
Sawan Kumar Sharma <sawansharmait@gmail.com>
2022-06-24 06:22:24 +0530

    Time: 463 ms (44.27%), Space: 6.5 MB (90.95%) - LeetHub

--- 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.cpp
+++ 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.cpp
@@ -1,33 +1,27 @@
 class Solution {
-    vector<int> dp;
     bool isPal(int i, int j, string& s){
         while(i<j){
-            if(s[i]==s[j]){
-                i++;
-                j--;
-            }
-            else return false;
+            if(s[i]!=s[j]) return false;
+            i++;
+            j--;
         }
         return true;
-    }
-    
-    int helper(string& s, int i, int j){
-        if(i==j) return 0;
-        if(dp[i]!=-1) return dp[i];
-        if(isPal(i, j, s)) return dp[i] = 0;
-        int ans = INT_MAX;
-        for(int k = i; k < j; k++){
-            if(isPal(i, k, s)){
-                int temp = 1 + helper(s, k+1, j);
-                ans = min(ans, temp);
-            }
-        }
-        return dp[i] = ans;
     }
 public:
     int minCut(string s) {
         int n = s.length();
-        dp.resize(n, -1);
-        return helper(s, 0, n-1);
+        vector<int> dp(n+1, -1);
+        dp[n] = 0;
+        for(int i = n-1; i >= 0; i--){
+            int ans = INT_MAX;
+            for(int k = i; k < n; k++){
+                if(isPal(i, k, s)){
+                    int temp = 1 + dp[k+1];
+                    ans = min(ans, temp);
+                }
+            }
+            dp[i] = ans;
+        }
+        return dp[0]-1;
     }
 };
