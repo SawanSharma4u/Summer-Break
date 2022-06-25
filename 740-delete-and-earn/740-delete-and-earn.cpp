dd03b4eebb8b4bb194229f8e8d1034ea69a839b5
Sawan Kumar Sharma <sawansharmait@gmail.com>
2022-06-25 10:05:06 +0530

    Time: 23 ms (51.33%), Space: 13.7 MB (53.56%) - LeetHub

--- 740-delete-and-earn/740-delete-and-earn.cpp
+++ 740-delete-and-earn/740-delete-and-earn.cpp
@@ -1,22 +1,19 @@
 class Solution {
-    unordered_map<int, int> points;
-    unordered_map<int, int> cache;
-    int maxPoint(int num){
-        if(num==0) return 0;
-        if(num==1) return points[1];
-        if(cache.count(num)) return cache[num];
-        return cache[num] = max(maxPoint(num-1), maxPoint(num-2)+points[num]);
-    }
-    
 public:
     int deleteAndEarn(vector<int>& nums) {
         int maxNumber = 0;
+        unordered_map<int, int> points;
         for(int i = 0; i < nums.size(); i++){
             points[nums[i]] += nums[i];
             if(maxNumber < nums[i]){
                 maxNumber = nums[i];
             }
         }
-        return maxPoint(maxNumber);
+        vector<int> maxPoint(maxNumber+1, 0);
+        maxPoint[1] = points[1];
+        for(int i = 2; i <= maxNumber; i++){
+            maxPoint[i] = max(maxPoint[i-1], maxPoint[i-2] + points[i]);
+        }
+        return maxPoint[maxNumber];
     }
 };
