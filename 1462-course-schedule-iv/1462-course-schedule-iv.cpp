f67ac70947c846d24f138fa2c656054602ba47a9
Sawan Kumar Sharma <sawansharmait@gmail.com>
2022-07-04 18:41:48 +0530

    Time: 868 ms (10.01%), Space: 116.7 MB (6.24%) - LeetHub

--- 1462-course-schedule-iv/1462-course-schedule-iv.cpp
+++ 1462-course-schedule-iv/1462-course-schedule-iv.cpp
@@ -2,36 +2,12 @@ class Solution {
 public:
     vector<bool> checkIfPrerequisite(int n, vector<vector<int>>& prerequisites, vector<vector<int>>& queries) {
         unordered_map<int, list<int>> adj;
-        vector<int> indegree(n);
         for(auto i : prerequisites){
             int u = i[0];
             int v = i[1];
             adj[u].push_back(v);
-            indegree[v]++;
         }
         int m = queries.size();
-        queue<int> q;
-        for(int i = 0; i < n; i++){
-            if(indegree[i]==0){
-                q.push(i);
-            }
-        }
-        // int cnt = 0;
-        // while(!q.empty()){
-        //     int front = q.front();
-        //     q.pop();
-        //     cnt++;
-        //     for(auto i : adj[front]){
-        //         indegree[i]--;
-        //         if(indegree[i]==0){
-        //             q.push(i);
-        //         }
-        //     }
-        // }
-        // if(cnt!=n){
-        //     vector<bool> ans(m, false);
-        //     return ans;
-        // }
         vector<bool> ans;
         vector<vector<int>> dp(n,vector<int>(n, -1));
         for(int i = 0; i < m; i++){
