e4751aa5634f04c9216193f93f092b55474f887a
Sawan Kumar Sharma <sawansharmait@gmail.com>
2022-07-04 18:46:38 +0530

    Time: 783 ms (14.51%), Space: 116.6 MB (6.34%) - LeetHub

--- 1462-course-schedule-iv/1462-course-schedule-iv.cpp
+++ 1462-course-schedule-iv/1462-course-schedule-iv.cpp
@@ -9,29 +9,37 @@ public:
         }
         int m = queries.size();
         vector<bool> ans;
+        vector<vector<int>> dp(n,vector<int>(n, -1));
         for(int i = 0; i < m; i++){
             int src = queries[i][0];
             int des = queries[i][1];
-            queue<int> q;
-            q.push(src);
-            vector<bool> visited(n, false);
-            visited[src] = true;
-            while(!q.empty()){
-                int front = q.front();
-                if(front==des){
-                    ans.push_back(true);
-                    break;
-                }
-                q.pop();
-                for(auto j : adj[front]){
-                    if(!visited[j]){
-                        visited[j] = true;
-                        q.push(j);
+            if(dp[src][des]!=-1){
+                if(dp[src][des]==1) ans.push_back(true);
+                else ans.push_back(false);
+            }
+            else{
+                queue<int> q;
+                q.push(src);
+                vector<bool> visited(n, false);
+                visited[src] = true;
+                while(!q.empty()){
+                    int front = q.front();
+                    dp[src][front] = 1;
+                    if(front==des){
+                        ans.push_back(true);
+                        break;
+                    }
+                    q.pop();
+                    for(auto j : adj[front]){
+                        if(!visited[j]){
+                            visited[j] = true;
+                            q.push(j);
+                        }
                     }
                 }
-            }
-            if(ans.size()==i){
-                ans.push_back(false);
+                if(ans.size()==i){
+                    ans.push_back(false);
+                }
             }
         }
         return ans;
