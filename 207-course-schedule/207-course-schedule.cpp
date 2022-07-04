1265ddd5e844b818b09b1f8d9b560d7b2f609ab0
Sawan Kumar Sharma <sawansharmait@gmail.com>
2022-07-04 14:44:09 +0530

    Time: 69 ms (7.04%), Space: 16.2 MB (9.38%) - LeetHub

--- 207-course-schedule/207-course-schedule.cpp
+++ 207-course-schedule/207-course-schedule.cpp
@@ -1,19 +1,19 @@
 class Solution {
-    bool dfs(unordered_map<int, list<int>>& adj, int src, vector<bool>& visited, vector<bool>& dfsvisited){
-        visited[src] = true;
-        dfsvisited[src] = true;
-        for(auto i : adj[src]){
-            if(!visited[i]){
-                bool ans = dfs(adj, i, visited, dfsvisited);
-                if(ans) return true;
-             }
-            else if(dfsvisited[i]){
-                return true;
-            }
-        }
-        dfsvisited[src] = false;
-        return false;
-    }
+    // bool dfs(unordered_map<int, list<int>>& adj, int src, vector<bool>& visited, vector<bool>& dfsvisited){
+    //     visited[src] = true;
+    //     dfsvisited[src] = true;
+    //     for(auto i : adj[src]){
+    //         if(!visited[i]){
+    //             bool ans = dfs(adj, i, visited, dfsvisited);
+    //             if(ans) return true;
+    //          }
+    //         else if(dfsvisited[i]){
+    //             return true;
+    //         }
+    //     }
+    //     dfsvisited[src] = false;
+    //     return false;
+    // }
 public:
     bool canFinish(int n, vector<vector<int>>& prerequisites) {
         unordered_map<int, list<int>> adj;
@@ -23,13 +23,39 @@ public:
             adj[u].push_back(v);
         }
         vector<bool> visited(n, false);
-        vector<bool> dfsvisited(n, false);
-        for(int i = 0; i < n; i++){
-            if(!visited[i]){
-                bool ans = dfs(adj, i, visited, dfsvisited);
-                if(ans == true) return false;
+        //vector<bool> dfsvisited(n, false);
+        vector<int> indegree(n, 0);
+        for(auto i : adj){
+            for(auto j : i.second){
+                indegree[j]++;
             }
         }
-        return true;
+        queue<int> q;
+        for(int i = 0; i < n; i++){
+            if(indegree[i]==0){
+                q.push(i);
+            }
+        }
+        int cnt = 0;
+        while(!q.empty()){
+            int front = q.front();
+            q.pop();
+            cnt++;
+            for(auto i : adj[front]){
+                indegree[i]--;
+                if(indegree[i]==0){
+                    q.push(i);
+                }
+            }
+        }
+        if(cnt == n) return true;
+        return false;
+        // for(int i = 0; i < n; i++){
+        //     if(!visited[i]){
+        //         bool ans = dfs(adj, i, visited, dfsvisited);
+        //         if(ans == true) return false;
+        //     }
+        // }
+        // return true;
     }
 };
