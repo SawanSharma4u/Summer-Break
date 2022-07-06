e81fdb384412da3bfc992cdb0123870eefaa74fc
Sawan Kumar Sharma <sawansharmait@gmail.com>
2022-07-06 22:34:39 +0530

    Time: 13 ms (22.89%), Space: 13.8 MB (47.65%) - LeetHub

--- 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.cpp
+++ 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.cpp
@@ -10,26 +10,37 @@
  * };
  */
 class Solution {
-    TreeNode* buildTree(vector<int>& preorder, int st, int ed){
-        if(st > ed) return NULL;
-        TreeNode * root = new TreeNode(preorder[st]);
-        int start = st+1;
-        int end = ed;
-        while(start <= end){
-            int mid = (start + end)/2;
-            if(preorder[mid] < preorder[st]){
-                start = mid+1;
-            }
-            else{
-                end = mid-1;
-            }
+    // TreeNode* buildTree(vector<int>& preorder, int st, int ed){
+    //     if(st > ed) return NULL;
+    //     TreeNode * root = new TreeNode(preorder[st]);
+    //     int start = st+1;
+    //     int end = ed;
+    //     while(start <= end){
+    //         int mid = (start + end)/2;
+    //         if(preorder[mid] < preorder[st]){
+    //             start = mid+1;
+    //         }
+    //         else{
+    //             end = mid-1;
+    //         }
+    //     }
+    //     root->left = buildTree(preorder, st+1, end);
+    //     root->right = buildTree(preorder, start, ed);
+    //     return root;
+    // }
+    TreeNode* buildTree2(vector<int>& preorder, int& i, int parent){
+        if(i == preorder.size() || preorder[i]>parent){
+            return NULL;
         }
-        root->left = buildTree(preorder, st+1, end);
-        root->right = buildTree(preorder, start, ed);
+        TreeNode* root = new TreeNode(preorder[i]);
+        i++;
+        root->left = buildTree2(preorder, i, root->val);
+        root->right = buildTree2(preorder, i, parent);
         return root;
     }
 public:
     TreeNode* bstFromPreorder(vector<int>& preorder) {
-        return buildTree(preorder, 0, preorder.size()-1);
+        int i = 0;
+        return buildTree2(preorder, i, INT_MAX);
     }
 };
