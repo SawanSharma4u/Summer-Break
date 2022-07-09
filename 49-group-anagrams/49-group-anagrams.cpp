d065a26e5c788427a067cc2564c1825734746e90
Sawan Kumar Sharma <sawansharmait@gmail.com>
2022-07-09 10:19:10 +0530

    Time: 59 ms (49.61%), Space: 20.7 MB (39.60%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.cpp
+++ 49-group-anagrams/49-group-anagrams.cpp
@@ -1,41 +1,53 @@
 class Solution {
-    static bool compare(string s1, string s2){
-        return s1.length() < s2.length();
-    }
+    // static bool compare(string s1, string s2){
+    //     return s1.length() < s2.length();
+    // }
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        int n = strs.size();
-        sort(strs.begin(), strs.end(), compare);
-        vector<vector<int>> ans(n, vector<int>(26, 0));
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < strs[i].length(); j++){
-                ans[i][strs[i][j]-'a']++;
-            }
+        unordered_map<string, vector<string>> map;
+        for(auto i : strs){
+            string temp = i;
+            sort(temp.begin(), temp.end());
+            map[temp].push_back(i);
         }
-        vector<vector<string>> op;
-        vector<bool> visited(n, false);
-        for(int i = 0; i < n; i++){
-            if(!visited[i]){
-                visited[i] = true;
-                vector<string> temp;
-                temp.push_back(strs[i]);
-                for(int j = i+1; j < n; j++){
-                    if(!visited[j]){
-                        int k;
-                        for(k = 0; k < 26; k++){
-                            if(ans[i][k]!=ans[j][k]){
-                                break;
-                            }
-                        }
-                        if(k==26){
-                            visited[j] = true;
-                            temp.push_back(strs[j]);
-                        }
-                    }
-                }
-                op.push_back(temp);
-            }
+        vector<vector<string>> ans;
+        for(auto i : map){
+            ans.push_back(i.second);
         }
-        return op;
+        return ans;
+        
+        // int n = strs.size();
+        // sort(strs.begin(), strs.end(), compare);
+        // vector<vector<int>> ans(n, vector<int>(26, 0));
+        // for(int i = 0; i < n; i++){
+        //     for(int j = 0; j < strs[i].length(); j++){
+        //         ans[i][strs[i][j]-'a']++;
+        //     }
+        // }
+        // vector<vector<string>> op;
+        // vector<bool> visited(n, false);
+        // for(int i = 0; i < n; i++){
+        //     if(!visited[i]){
+        //         visited[i] = true;
+        //         vector<string> temp;
+        //         temp.push_back(strs[i]);
+        //         for(int j = i+1; j < n; j++){
+        //             if(!visited[j]){
+        //                 int k;
+        //                 for(k = 0; k < 26; k++){
+        //                     if(ans[i][k]!=ans[j][k]){
+        //                         break;
+        //                     }
+        //                 }
+        //                 if(k==26){
+        //                     visited[j] = true;
+        //                     temp.push_back(strs[j]);
+        //                 }
+        //             }
+        //         }
+        //         op.push_back(temp);
+        //     }
+        // }
+        // return op;
     }
 };
