2d1a228530172f1aab570b7abe012aee82447225
Sawan Kumar Sharma <sawansharmait@gmail.com>
2022-07-16 20:52:24 +0530

    Time: 5 ms (75.14%), Space: 7 MB (62.96%) - LeetHub

--- 10-regular-expression-matching/10-regular-expression-matching.cpp
+++ 10-regular-expression-matching/10-regular-expression-matching.cpp
@@ -1,46 +1,67 @@
 class Solution {
-    vector<vector<int>> dp;
-    bool help(string &s, string &p, int i, int j){
-        if(i==s.length()){
-            while(j!=p.length()){
-                if(p[j]=='*')
-                    j++;
-                else if(j+1<p.length() && p[j+1]=='*'){
-                    j = j+2;
-                }
-                else break;
-            }
-            if(j==p.length()) return true;
-            return false;
-        }
-        if(j == p.length()) return false;
+//     vector<vector<int>> dp;
+//     bool help(string &s, string &p, int i, int j){
+//         if(i==s.length()){
+//             while(j!=p.length()){
+//                 if(p[j]=='*')
+//                     j++;
+//                 else if(j+1<p.length() && p[j+1]=='*'){
+//                     j = j+2;
+//                 }
+//                 else break;
+//             }
+//             if(j==p.length()) return true;
+//             return false;
+//         }
+//         if(j == p.length()) return false;
         
-        if(dp[i][j]!=-1) return dp[i][j];
-        bool ans = false;
-        if(s[i] == p[j] || p[j]=='.'){
-            bool a = help(s,p, i+1, j+1);
-            bool b = false;
-            if(j+1!=p.length() && p[j+1]=='*'){
-                b = help(s, p, i, j+2);
-            }
-            ans = a || b;
-        }
-        else if(p[j]=='*'){
-            if(j!=0 && (s[i]==p[j-1] || p[j-1]=='.')){
-                ans = help(s, p, i+1, j) || help(s, p, i, j+1);
-            }
-            else{
-                ans = help(s, p, i, j+1);
-            }
-        }
-        else if(j!=p.length()-1 && p[j+1] == '*'){
-            ans = help(s, p, i, j+2);
-        }
-        return dp[i][j] = ans;
-    }
+//         if(dp[i][j]!=-1) return dp[i][j];
+//         bool ans = false;
+//         if(s[i] == p[j] || p[j]=='.'){
+//             bool a = help(s,p, i+1, j+1);
+//             bool b = false;
+//             if(j+1!=p.length() && p[j+1]=='*'){
+//                 b = help(s, p, i, j+2);
+//             }
+//             ans = a || b;
+//         }
+//         else if(p[j]=='*'){
+//             if(j!=0 && (s[i]==p[j-1] || p[j-1]=='.')){
+//                 ans = help(s, p, i+1, j) || help(s, p, i, j+1);
+//             }
+//             else{
+//                 ans = help(s, p, i, j+1);
+//             }
+//         }
+//         else if(j!=p.length()-1 && p[j+1] == '*'){
+//             ans = help(s, p, i, j+2);
+//         }
+//         return dp[i][j] = ans;
+//     }
 public:
     bool isMatch(string s, string p) {
-        dp.resize(s.length(), vector<int>(p.length(), -1));
-        return help(s, p, 0, 0);
+        vector<vector<bool>> dp(s.length()+1, vector<bool>(p.length()+1));
+        dp[0][0] = true;
+        for(int i = 1; i <= p.length(); i++){
+            if(p[i-1]=='*'){
+                dp[0][i] = dp[0][i-2];
+            }
+            else dp[0][i] = false;
+        }
+        for(int i = 1; i <= s.length(); i++){
+            for(int j = 1; j <= p.length(); j++){
+                if(s[i-1]==p[j-1] || p[j-1]=='.'){
+                    dp[i][j] = dp[i-1][j-1];
+                }
+                else if(p[j-1]=='*'){
+                    dp[i][j] = dp[i][j-2];
+                    if(p[j-2]=='.' || p[j-2]==s[i-1]){
+                        dp[i][j] = dp[i][j] || dp[i-1][j];
+                    }
+                }
+                else dp[i][j] = false;
+            }
+        }
+        return dp[s.length()][p.length()];
     }
 };
