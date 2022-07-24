e1a15cac4fdca4db31b0118afb870d83a8079ea6
Sawan Kumar Sharma <sawansharmait@gmail.com>
2022-07-25 00:18:50 +0530

    Time: 190 ms (42.80%), Space: 14.9 MB (21.86%) - LeetHub

---
+++ 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+    int m = matrix.size(), n = matrix[0].size();
+    int  i=0, j = n-1;  // check "right top" most.. in this case we choose it.. or "left Bottom"
+    
+    while (i <= m-1 && j >= 0)   // Please check it clearly.. Is their in Limit..
+    {
+        if (matrix[i][j] == target)  //  if cur == target then found..
+        {
+            return true;
+        }
+        else if (matrix[i][j] > target)  // if cur greater than target then 'move left wards'..
+        {
+            j--;
+        }
+        else if (matrix[i][j] < target) //  if cur lesser than target then 'move down wards'..
+        {
+            i++;
+        }            
+    }
+    return false;
+}
+};
