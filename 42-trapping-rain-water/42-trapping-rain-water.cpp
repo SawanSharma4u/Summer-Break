b7a21743d95527ada4a62c14e87d4a82cec8bf70
Sawan Kumar Sharma <sawansharmait@gmail.com>
2022-07-26 15:44:38 +0530

    Time: 27 ms (41.59%), Space: 19.7 MB (90.14%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.cpp
+++ 42-trapping-rain-water/42-trapping-rain-water.cpp
@@ -1,6 +1,33 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
+        int Left_max = 0;
+        int Right_max = 0;
+        int i = 0, j = height.size()-1;
+        int ans = 0;
+        while(i<=j){
+            if(Left_max <= Right_max){
+                if(Left_max < height[i]){
+                    Left_max = height[i];
+                }
+                else{
+                    ans += Left_max - height[i];
+                }
+                i++;
+            }
+            else{
+                if(Right_max < height[j]){
+                    Right_max = height[j];
+                }
+                else{
+                    ans += Right_max - height[j];
+                }
+                j--;
+            }
+        }
+        return ans;
+        
+        
         // vector<int> prev;
         // prev.push_back(0);
         // int n = height.size();
@@ -22,21 +49,21 @@ public:
         // }
         // return ans;
         
-        stack<int> s;
-        int ans = 0;
-        int n = height.size();
-        for(int i = 0; i < n; i++){
-            while(!s.empty() && height[i] > height[s.top()]){
-                int top = s.top();
-                s.pop();
-                if(!s.empty()){
-                    int distance = i - s.top() -1;
-                    int bounded_height = min(height[s.top()], height[i])-height[top];
-                    ans += distance*bounded_height;
-                }
-            }
-            s.push(i);
-        }
-        return ans;
+        // stack<int> s;
+        // int ans = 0;
+        // int n = height.size();
+        // for(int i = 0; i < n; i++){
+        //     while(!s.empty() && height[i] > height[s.top()]){
+        //         int top = s.top();
+        //         s.pop();
+        //         if(!s.empty()){
+        //             int distance = i - s.top() -1;
+        //             int bounded_height = min(height[s.top()], height[i])-height[top];
+        //             ans += distance*bounded_height;
+        //         }
+        //     }
+        //     s.push(i);
+        // }
+        // return ans;
     }
 };
