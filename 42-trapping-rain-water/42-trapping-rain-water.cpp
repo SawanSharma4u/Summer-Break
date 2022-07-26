faf8346c2386d5f5085d17587bead36da4ac0d6c
Sawan Kumar Sharma <sawansharmait@gmail.com>
2022-07-26 14:59:01 +0530

    Time: 30 ms (30.62%), Space: 20.3 MB (38.63%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.cpp
+++ 42-trapping-rain-water/42-trapping-rain-water.cpp
@@ -1,24 +1,41 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        vector<int> prev;
-        prev.push_back(0);
-        int n = height.size();
-        vector<int> next(n);
-        int temp = height[0];
-        for(int i = 0; i < n-1; i++){
-            temp = max(height[i], temp);
-            prev.push_back(temp);
-        }
-        next[n-1] = 0;
-        temp = height[n-1];
-        for(int i = n-1; i > 0; i--){
-            temp = max(height[i], temp);
-            next[i-1] = temp;
-        }
+        // vector<int> prev;
+        // prev.push_back(0);
+        // int n = height.size();
+        // vector<int> next(n);
+        // int temp = height[0];
+        // for(int i = 0; i < n-1; i++){
+        //     temp = max(height[i], temp);
+        //     prev.push_back(temp);
+        // }
+        // next[n-1] = 0;
+        // temp = height[n-1];
+        // for(int i = n-1; i > 0; i--){
+        //     temp = max(height[i], temp);
+        //     next[i-1] = temp;
+        // }
+        // int ans = 0;
+        // for(int i = 0; i < n; i++){
+        //     ans += max(0, min(next[i],prev[i])-height[i]);
+        // }
+        // return ans;
+        
+        stack<int> s;
         int ans = 0;
+        int n = height.size();
         for(int i = 0; i < n; i++){
-            ans += max(0, min(next[i],prev[i])-height[i]);
+            while(!s.empty() && height[i] > height[s.top()]){
+                int top = s.top();
+                s.pop();
+                if(!s.empty()){
+                    int distance = i - s.top() -1;
+                    int bounded_height = min(height[s.top()], height[i])-height[top];
+                    ans += distance*bounded_height;
+                }
+            }
+            s.push(i);
         }
         return ans;
     }
