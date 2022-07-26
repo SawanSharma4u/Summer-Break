db2023e5fba067a3f817c24799ab3ca287808fea
Sawan Kumar Sharma <sawansharmait@gmail.com>
2022-07-26 08:57:40 +0530

    Time: 22 ms (8.64%), Space: 13.7 MB (17.99%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,30 +1,49 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        vector<int> ans(2,-1);
-        if(nums.size()==0)
+        vector<int> ans;
+        if(nums.size()==0){
+            ans.resize(2,-1);
             return ans;
-        int start = 0, end = nums.size()-1;
-        while(start<end){
-            int mid = (start+end)/2;
-            if(nums[mid]<target)
-                start = mid + 1;
-            else
-                end = mid;
         }
-        if(nums[start]!=target)
+        int i = 0, j = nums.size()-1;
+        int y=-1;
+        while(i <= j){
+            int mid = (i + j)/2;
+            if(nums[mid] == target){
+                y = j;
+                break;
+            }
+            else if(nums[mid] < target){
+                i = mid+1;
+            }
+            else j = mid-1;
+        }
+        while(i <= j){
+            int mid = (i+j)/2;
+            if(nums[mid] >= target){
+                j = mid-1;
+            }
+            else{
+                i = mid+1;
+            }
+        }
+        if(i>=0 && i < nums.size() && nums[i]==target) ans.push_back(i);
+        else {
+            ans.resize(2,-1);
             return ans;
-        else
-            ans[0]=start;
-        end = nums.size()-1;
-        while(start<end){
-            int mid = (start + end)/2 + 1;
-            if(nums[mid]>target)
-                end = mid - 1;
-            else
-                start = mid;
         }
-        ans[1]=start;
+        while(i<=y){
+            int mid = (i+y)/2;
+            if(nums[mid] <= target){
+                i = mid+1;
+            }
+            else{
+                y = mid-1;
+            }
+        }
+        int upper_bound;
+        ans.push_back(y);
         return ans;
     }
 };
