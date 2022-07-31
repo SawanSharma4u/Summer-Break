ebac9b2a85d0a5a19bbae88a11f2147fd97fe87f
Sawan Kumar Sharma <sawansharmait@gmail.com>
2022-07-31 23:40:31 +0530

    Time: 138 ms (23.56%), Space: 52.2 MB (70.00%) - LeetHub

--- 795-number-of-subarrays-with-bounded-maximum/795-number-of-subarrays-with-bounded-maximum.cpp
+++ 795-number-of-subarrays-with-bounded-maximum/795-number-of-subarrays-with-bounded-maximum.cpp
@@ -1,32 +1,11 @@
 class Solution {
 public:
     int numSubarrayBoundedMax(vector<int>& nums, int l, int r) {
-        vector<int> left(nums.size());
+        int ans = 0, left = -1, right=-1;
         for(int i = 0; i < nums.size(); i++){
-           if(nums[i] >= l){
-               if(i!=0 && nums[i-1] < l) left[i] = left[i-1];
-               else left[i] = i;
-           }
-            else{
-                if(i==0 || nums[i-1] >= l) left[i] = i;
-                else left[i] = left[i-1];
-            }
-        }
-        vector<int> right(nums.size());
-        for(int i = nums.size()-1; i >= 0; i--){
-            if(nums[i] <= r){
-                if(i==nums.size()-1) right[i] = nums.size();
-                else right[i] = right[i+1];
-            }
-            else{
-                right[i] = i;
-            }
-        }
-        int ans = 0;
-        for(int i = 0; i < nums.size(); i++){
-            if(nums[i] >= l && nums[i] <= r){
-                ans += (i-left[i]+1)*(right[i]-i);
-            }
+            if(nums[i] > r) left = i;
+            if(nums[i] >= l) right = i;
+            ans += right-left;
         }
         return ans;
     }
